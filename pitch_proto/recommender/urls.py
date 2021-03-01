ac714719a64a9ffad535edba864c187ccbbd09c5
Edward B. Callihan <edward.callihan@gmail.com>
2021-02-28 23:11:20 -0500

    project code from 2/28/2021 live-coding

---
+++ pitch_proto/recommender/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# app_name = 'recommender'
+
+urlpatterns = [
+    path('best/', views.searchform_get, name='best'),
+    path('bestp/', views.searchform_post, name='bestp'),
+]
