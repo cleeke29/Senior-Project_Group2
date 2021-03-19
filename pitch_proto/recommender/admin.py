07b88c2481a69a588c56bcecc8144c22a8cdd82e
David Rupell <45103517+drupell@users.noreply.github.com>
2021-03-19 16:31:29 -0400

    Update admin.py

--- pitch_proto/recommender/admin.py
+++ pitch_proto/recommender/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Musicdata
 
 # Register your models here.
-admin.site.register(Musicdata)
+admin.site.register(Categories)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(AudioFeatures)
+admin.site.register(Track)
+admin.site.register(Genres)
