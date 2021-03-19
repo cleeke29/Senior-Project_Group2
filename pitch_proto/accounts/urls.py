c9a053e80e9e33c8c5c91c9e0036c7ef78a4fd77
David Rupell <45103517+drupell@users.noreply.github.com>
2021-03-19 17:23:57 -0400

    Update urls.py
    Add path to friends page.

--- pitch_proto/accounts/urls.py
+++ pitch_proto/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from .views import SignUpView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [ 
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('friends/', TemplateView.as_view(template_name='friends.html'), name='friends'),
 ]
