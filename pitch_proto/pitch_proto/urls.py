f2b08c1c65b49033f3e8c2720003f72ce550c1bf
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-03-21 16:08:22 -0400

    Update urls.py
    Added paths to recommendation test and settings, these will not work until the recommendationTest app and the settings app have been added

--- pitch_proto/pitch_proto/urls.py
+++ pitch_proto/pitch_proto/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('recommender/', include('recommender.urls')),
-    path('group/', include('partyroom.urls'))
+    path('group/', include('partyroom.urls')),
+    path('recommendation/', include('recommendationTest.urls')),
+    path('settings/', include('settings.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
