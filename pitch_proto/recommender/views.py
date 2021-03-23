a4fabf8dc544f50bddb23a7b87e31e7ecffbe4ee
KeiganPerryMatson <71092572+KeiganPerryMatson@users.noreply.github.com>
2021-03-23 16:37:38 -0400

    Update views.py for Advanced Search
    Update views to include handling for fields from wireframe.

--- pitch_proto/recommender/views.py
+++ pitch_proto/recommender/views.py
@@ -7,14 +7,28 @@ from django.views.decorators.http import require_POST, require_GET
 import numpy as np
 
 
-def find_albums(artist, from_year = None, to_year = None):
+
+def find_albums(song = None, artist = None, album = None, from_year = None, to_year = None):
     query = Track.objects.filter(artists__icontains = artist)
+    
+    if song is not None:
+        query = query.filter(track_name__icontains = song)
+
+    if album is not None:
+        query = query.filter(album_name__icontains = album)
+
     if from_year is not None:
         query = query.filter(year__gte = from_year)
+
     if to_year is not None:
         query = query.filter(year__lte = to_year)
+
+    # if genre is not None:
+    #     query = 
+
     return list(query.order_by('-track_popularity').values('track_id','track_name','year'))
-    
+
+
 
 @require_POST
 def searchform_post(request):
@@ -23,19 +37,31 @@ def searchform_post(request):
     # check whether it's valid:
     if form.is_valid():
         # process the data in form.cleaned_data as required
+        song = None if form.cleaned_data['song'] == None else form.cleaned_data['song']
+        artist = None if form.cleaned_data['artist'] == None else form.cleaned_data['artist']
+        album = None if form.cleaned_data['album'] == None else form.cleaned_data['album']
         from_year = None if form.cleaned_data['from_year'] == None else int(form.cleaned_data['from_year'])
         to_year = None if form.cleaned_data['to_year'] == None else int(form.cleaned_data['to_year'])
+        # genre = None if form.cleaned_data['genre'] == None else form.cleaned_data['genre']
+        number_of_songs = None if form.cleaned_data['number_of_songs'] == None else int(form.cleaned_data['number_of_songs'])
+
         albums = find_albums(
-                form.cleaned_data['artist'],
+                song,
+                artist,
+                album,
                 from_year,
                 to_year
             )
             
-        # Random 3 of top 10 popular albums
-        albums = list(np.random.permutation(albums[:10]))[:3] 
+        if number_of_songs is not None:
+            albums = list(np.random.permutation(albums[:10]))[:number_of_songs] 
+        else:
+            albums = list(np.random.permutation(albums[:10]))[:3]
+         
         return render(request, 'recommender/searchform.html', {'form': form, 'albums': albums })
     else:
         raise Http404('Something went wrong')
+
 
 
 @require_GET
