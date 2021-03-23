5fe871b4e535e1634cbe8c73f3af9e7aa8b73123
KeiganPerryMatson <71092572+KeiganPerryMatson@users.noreply.github.com>
2021-03-23 16:36:48 -0400

    Update forms.py for Advanced Search
    Update form to include fields from wireframe.

--- pitch_proto/recommender/forms.py
+++ pitch_proto/recommender/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    artist = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
+    song = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}), required=False)
+    artist = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}), required=False)
+    album = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}), required=False)
     from_year = forms.IntegerField(required=False)
     to_year = forms.IntegerField(required=False)
+    # genre = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
+    number_of_songs = forms.IntegerField(required=False)
