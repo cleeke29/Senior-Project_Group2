7170fc7fd59521e9377ed4e4c1abedaabaafbc1c
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-03-31 15:55:01 -0400

    Update views.py
    Added change username functionality and reset preferred music

--- pitch_proto/settings/views.py
+++ pitch_proto/settings/views.py
@@ -14,11 +14,24 @@ def settingsUpdate(request):
 	if form.is_valid():
 		Dark = form.cleaned_data['DarkMode']
 		Explicit = form.cleaned_data['Explicit']
+		username = form.cleaned_data['newUser']
 	conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
 	cursor = conn.cursor()
-	query = 'update accounts_user set dark_mode = ' + str(Dark) + ' where id = ' + str(user.id)
+	query = 'update accounts_user set dark_mode = ' + str(Dark) + ', explicit = '+ str(Explicit) + ' where id = ' + str(user.id)
 	cursor.execute(query)
+	if username is not '':
+		query = "update accounts_user set username = '" + username + "' where id = " + str(user.id)
+		cursor.execute(query)
 	conn.commit()
 	form = settingForm()
 	return render(request, 'settings.html', {'form' : form})
 
+def settingsReset(request):
+	print('in')
+	form = settingForm()
+	conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+	cursor = conn.cursor()
+	query = 'update "recommendationTest_preferredmusic" set "danceabilityPreferred" = 0.5175604065506179, "acousticnessPreferred" = 0.5124401243104162, "tempoPreferred" = 116.89153340585992 where id = ' + str(request.user.id)
+	cursor.execute(query)
+	conn.commit()
+	return render(request, 'settings.html', {'form' : form})
