f9270eb75880683a5fd07d7fb4ed8e708ced21b8
David Rupell <45103517+drupell@users.noreply.github.com>
2021-04-02 17:30:06 -0400

    Add form for Friend page entry box
    Adds required form to add functionality to friends page "Add Friend" form. Uses only a username field.

--- pitch_proto/accounts/forms.py
+++ pitch_proto/accounts/forms.py
@@ -10,3 +10,7 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields =('username', 'email')
+
+class AddFriendForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}), max_length=150, required=False)
+        
