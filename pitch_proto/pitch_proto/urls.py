b2b85f48706caaccff056c115129ba3f807a3bed
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-02 15:47:22 -0400

    Update urls.py
    Fixed home page call

--- pitch_proto/pitch_proto/urls.py
+++ pitch_proto/pitch_proto/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import  static
 from . import settings
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.homepage, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
