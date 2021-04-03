710e71ec72bf2086bcde3e0b4c73a867517581cf
KeiganPerryMatson <71092572+KeiganPerryMatson@users.noreply.github.com>
2021-04-03 17:01:28 -0400

    Update urls.py
    Add Ed's changes for advanced search results

--- pitch_proto/recommender/urls.py
+++ pitch_proto/recommender/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('advanced-search/', views.searchform_get, name='advanced-search'),
     path('advanced-results/', views.searchform_post, name='advanced-results'),
+    path('results/', views.results, name='results'),
+    # path('album-info/', views.display_album, name='album-info'),
 ]
