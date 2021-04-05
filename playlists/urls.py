3cdb6915df57daa727330e6757ce5886873971e0
CharlesMossbrucker <78179672+CharlesMossbrucker@users.noreply.github.com>
2021-04-05 18:41:27 -0400

    Delete urls.py

--- playlists/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('playlists/', views.playlists, name='playlists'),
-    path('newplaylists/', views.newPlaylist, name='newPlaylist'),
-    
-]
