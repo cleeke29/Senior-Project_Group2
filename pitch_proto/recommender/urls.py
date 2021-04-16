beea0e6a12d7e4e4b70b3f15e44e6adc9dea4c58
cleeke29 <72581360+cleeke29@users.noreply.github.com>
2021-04-16 16:20:14 -0400

    Delete urls.py

--- pitch_proto/recommender/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# app_name = 'recommender'
-
-urlpatterns = [
-    path('best/', views.searchform_get, name='best'),
-    path('bestp/', views.searchform_post, name='bestp'),
-]
