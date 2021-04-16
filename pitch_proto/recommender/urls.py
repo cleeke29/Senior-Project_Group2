f7e51f1ac405f9027d5f390619c8e9165c5f9683
cleeke29 <72581360+cleeke29@users.noreply.github.com>
2021-04-16 16:20:49 -0400

    Replace files

---
+++ pitch_proto/recommender/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# app_name = 'recommender'
+
+urlpatterns = [
+    path('advanced-search/', views.searchform_get, name='advanced-search'),
+    path('advanced-results/', views.searchform_post, name='advanced-results'),
+    path('results/', views.results, name='results'),
+    # path('results/<str:newPlaylist>/', views.results, name='advanced-results'),
+    # path('album-info/', views.display_album, name='album-info'),
+]
