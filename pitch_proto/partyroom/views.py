2b43f67171c3316d023324b9fc47d582896cefaf
ECallihan <edward.callihan@gmail.com>
2021-04-16 15:55:26 -0400

    adjustment to partyroom

--- pitch_proto/partyroom/views.py
+++ pitch_proto/partyroom/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 import requests
 
 def partyroom(request):
-    return render(request, 'partyroom/partyroom2.html')
+    return render(request, 'partyroom/partyroom.html')
 
 # Create your views here.
