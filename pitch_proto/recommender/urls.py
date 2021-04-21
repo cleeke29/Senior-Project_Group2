2164d831a1ef90bd75aa7115a41dc4a37e6214de
SimranBenipal <71046507+SimranBenipal@users.noreply.github.com>
2021-04-21 17:27:20 -0400

    add URLs

--- pitch_proto/recommender/urls.py
+++ pitch_proto/recommender/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('advanced-search/', views.searchform_get, name='advanced-search'),
     path('advanced-results/', views.searchform_post, name='advanced-results'),
     path('results/', views.results, name='results'),
+    path('search/<str:song_name>/', views.searchSong, name="search_song"),
     # path('album-info/', views.display_album, name='album-info'),
 ]
