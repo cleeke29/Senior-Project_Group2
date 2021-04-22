3f7107c8696564c52c05d780b1ea2128cd800d42
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-22 15:55:47 -0400

    Fixed forms in settings, third times the charm

--- pitch_proto/settings/forms.py
+++ pitch_proto/settings/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class settingForm(forms.Form):
 	DarkMode = forms.BooleanField(required = False)
 	Explicit = forms.BooleanField(required = False)
+	newUser = forms.CharField(required = False)
