7c14ae1464851aa6d9f823a3cec0497d75a9286d
CharlesMossbrucker <78179672+CharlesMossbrucker@users.noreply.github.com>
2021-04-22 13:57:50 -0400

    add new urls for new playlist functionality

--- pitch_proto/playlists/urls.py
+++ pitch_proto/playlists/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [ 
     path('playlists/', views.playlists, name='playlists'),
-    path('newplaylists/', views.newPlaylist, name='newPlaylist'),
+    path('newplaylist/', views.newPlaylist, name='newplaylist'),
+    path('playlists/<str:list>/', views.displayPlaylist, name='displayplaylist'),
+    path('playlists/<str:list>/remove/<str:song>/', views.removeSong, name='removesong'),
+    path('playlists/remove/<str:list>/', views.removePlaylist, name='removePlaylist'),
+    path('playlists/copyNewPlaylist/<str:list>/<int:id>/', views.copyPlaylist, name='copyplaylist'),
     
 ]
