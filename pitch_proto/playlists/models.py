4e1e8c904740f5c81a4b6f9899fc3258f58411af
CharlesMossbrucker <78179672+CharlesMossbrucker@users.noreply.github.com>
2021-04-22 13:59:04 -0400

    fixed the songs field of playlist model

--- pitch_proto/playlists/models.py
+++ pitch_proto/playlists/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from accounts.models import User
+from recommender.models import Track
 
 class playlist(models.Model):
     description = models.TextField()
-    song_ids = models.ManyToManyField("playlist", blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-
-
-# Create your models here.
+    songs = models.ManyToManyField(Track)
