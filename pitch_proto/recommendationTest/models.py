37238cc738ae97ed7a6b50a610475ccf99efd309
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-23 13:57:42 -0400

    Added new quickRec model

--- pitch_proto/recommendationTest/models.py
+++ pitch_proto/recommendationTest/models.py
@@ -1,30 +1,16 @@
 from django.db import models
-
-# class Musicdata(models.Model):
-#     acousticness = models.FloatField()
-#     artists = models.TextField()
-#     danceability = models.FloatField()
-#     duration_ms = models.FloatField()
-#     energy = models.FloatField()
-#     explicit = models.FloatField()
-#     id = models.TextField(primary_key=True)
-#     instrumentalness = models.FloatField()
-#     key = models.FloatField()
-#     liveness = models.FloatField()
-#     loudness = models.FloatField()
-#     mode = models.FloatField()
-#     name = models.TextField()
-#     popularity = models.FloatField()
-#     release_date = models.DateField()
-#     speechiness = models.FloatField()
-#     tempo = models.FloatField()
-#     valence = models.FloatField()
-#     year = models.IntegerField()
+from django.contrib.postgres.fields import ArrayField
 
 class preferredMusic(models.Model):
     danceabilityPreferred = models.FloatField(blank = True)
     acousticnessPreferred = models.FloatField(blank = True)
     tempoPreferred = models.FloatField(blank = True)
+    energyPreferred = models.FloatField(blank = True)
+    loudnessPreferred = models.FloatField(blank = True)
+    speechinessPreferred = models.FloatField(blank = True)
+    instrumentalnessPreferred = models.FloatField(blank = True)
+    livenessPreferred = models.FloatField(blank = True)
+    valencePreferred = models.FloatField(blank = True)
     lowSongId = models.TextField(null = True)
     midSongId = models.TextField(null = True)
     highSongId = models.TextField(null = True)
@@ -33,3 +19,8 @@ class holdMyData(models.Model):
     lowSongId = models.TextField()
     midSongId = models.TextField()
     highSongId = models.TextField()
+
+class quickRec(models.Model):
+    user_id = models.CharField(primary_key = True, max_length = 100)
+    songs = ArrayField(models.TextField(null = True))
+
