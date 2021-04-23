11cfdd567885fbe70b46a6c3323eea70f1a5c39a
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-23 14:09:51 -0400

    Added both discussions and song/album info pages

--- pitch_proto/recommender/views.py
+++ pitch_proto/recommender/views.py
@@ -146,6 +146,119 @@ def searchSong(request, song_name):
     request.session['export_query'] = args
     return redirect('results')
 
+class commentObject:
+    def __init__(self, username, text):
+        self.username = username
+        self.text = text
+
+def getComments(track):
+    conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+    cursor = conn.cursor()
+    query = "select * from recommender_comment where track_id = '" + str(track) + "' order by commentid DESC"
+    cursor.execute(query)
+    return cursor.fetchall()
+def discuss(request, track):
+    conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+    cursor = conn.cursor()
+    query = "select exists(select 1 from recommender_comment where track_id = '" + str(track) + "')"
+    cursor.execute(query)
+    exists = cursor.fetchone()
+    form = commentForm()
+    query = "select track_name from recommender_track where track_id = '" + str(track) + "'"
+    cursor.execute(query)
+    data = cursor.fetchone()
+    if(exists[0]):
+        comments = getComments(track)
+        usernames = []
+        texts = []
+        for entry in comments:
+            usernames.append(entry[1])
+            texts.append(entry[2])
+        commentarray = []
+        x = len(comments) - 1
+        while(x >= 0):
+            commentarray.append(commentObject(usernames[x], texts[x]))
+            x = x - 1
+        return render(request, 'recommender/discuss.html', {'exists' : 'True', 'track_id' : track, 'form' : form, 'track_name' : data[0], 'comments' : commentarray})
+    return render(request, 'recommender/discuss.html', {'exists' : 'False', 'track_id' : track, 'form' : form, 'track_name' : data[0]})
+
+def startDiscuss(request, track):
+    form = commentForm(request.POST)
+    text = ''
+    if form.is_valid():
+        text = form.cleaned_data['comment']
+    conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+    cursor = conn.cursor()
+    query = "insert into recommender_comment (track_id, username, text) values ('" + str(track)  + "', '" + str(request.user.username) + "', '" + str(text) + "')"
+    cursor.execute(query)
+    conn.commit()
+    query = "select track_name from recommender_track where track_id = '" + str(track) + "'"
+    cursor.execute(query)
+    data = cursor.fetchone()
+    comments = getComments(track)
+    usernames = []
+    texts = []
+    for entry in comments:
+        usernames.append(entry[1])
+        texts.append(entry[2])
+    commentarray = []
+    x = len(comments) - 1
+    while(x >= 0):
+        commentarray.append(commentObject(usernames[x], texts[x]))
+        x = x - 1
+    return render(request, 'recommender/discuss.html', {'exists' : 'True', 'track_id' : track, 'form' : form, 'track_name' : data[0], 'comments' : commentarray})
+    
+
+def Song_info(request, track):
+    conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+    cursor = conn.cursor()
+    query = "select * from recommender_track where track_id = '" + str(track) + "'"
+    cursor.execute(query)
+    data = cursor.fetchone()
+    artists = re.findall(r"\'(.*?)\'", data[0])
+    return render(request, 'recommender/browse.html', { 'artists' : artists, 
+                                                        'track_id' : track,
+                                                        'track_name' : data[1],
+                                                        'album_name' : data[3],
+                                                        'explicit' : data[4],
+                                                        'years' : data[6],
+                                                        'track_number' : data[7]})
+
+def Album_info(request, album):
+    conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+    cursor = conn.cursor()
+    query = "select DISTINCT on (track_number) track_name, track_id, artists, explicit, year, track_number from recommender_track where album_name = '" + album + "'" 
+    cursor.execute(query)
+    data = cursor.fetchall()
+    names = []
+    ids = []
+    artists = []
+    explicits = []
+    years = []
+    track_numbers = []
+    for entry in data:
+        names.append(entry[0])
+        ids.append(entry[1])
+        artists.append(re.findall(r"\'(.*?)\'", entry[2]))
+        explicits.append(entry[3])
+        years.append(entry[4])
+        track_numbers.append(entry[5])
+    class songObject:
+        def __init__(self, name, id, artist, explicit, year, track_number):
+            self.name = name
+            self.id = id
+            self.artist = artist
+            self.explicit = explicit
+            self.year = year
+            self.track_number = track_number
+
+    newData = []
+    x = 13
+    while (x >= 0):
+        newData.append(songObject(names[x], ids[x], artists[x], explicits[x], years[x], track_numbers[x]))
+        x = x - 1
+        #'names' : names, 'ids' : ids, 'artists' : artists, 'explicits' : explicits, 'years' : years, 'track_numbers' : track_numbers
+    return render(request, 'recommender/album_info.html', {'data' : newData, 'album' : album})
 # @require_POST
 # def display_album(request):
 #     conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
