f52ab55d73c464297d985ec4bb92b80e2b39c1ba
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-23 14:11:25 -0400

    Added urls necessary for discussions and browsing

--- pitch_proto/recommender/urls.py
+++ pitch_proto/recommender/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('advanced-results/', views.searchform_post, name='advanced-results'),
     path('results/', views.results, name='results'),
     path('search/<str:song_name>/', views.searchSong, name="search_song"),
+    path('Song_info/<str:track>/', views.Song_info, name='Song_info'),
+    path('Album_info/<str:album>/', views.Album_info, name='Album_info'),
+    path('song_discuss/<str:track>/', views.discuss, name='discuss'),
+    path('start_discuss/<str:track>/', views.startDiscuss, name='startDiscuss'),
     # path('album-info/', views.display_album, name='album-info'),
 ]
