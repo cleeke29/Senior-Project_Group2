ea5655670fed516dc21265a5759cbb5061907087
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-23 14:02:49 -0400

    Recs on home now conform to new method

--- pitch_proto/pitch_proto/views.py
+++ pitch_proto/pitch_proto/views.py
@@ -6,11 +6,6 @@ import recommendationTest.views
 def homepage(request):
 	if request.user.is_authenticated:
 		recommendationTest.views.tableEntry(request.user.id)
-		dance = recommendationTest.views.getRecs('danceability', request.user.id)
-		dance2 = recommendationTest.views.getRecs('danceability', request.user.id)
-		acoustic = recommendationTest.views.getRecs('acousticness', request.user.id)
-		acoustic2 = recommendationTest.views.getRecs('acousticness', request.user.id)
-		tempo = recommendationTest.views.getRecs('tempo', request.user.id)
-		tempo2 = recommendationTest.views.getRecs('tempo', request.user.id)
-		return render(request, 'home.html', {'dance' : dance, 'acoustic' : acoustic, 'tempo' : tempo, 'dance2' : dance2, 'acoustic2' : acoustic2, 'tempo2' : tempo2})
+		songids = recommendationTest.views.grabQuickRec(request.user.id)
+		return render(request, 'home.html', {'songids' : songids[0][0]})
 	return render(request, 'home.html')
