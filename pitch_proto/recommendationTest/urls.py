387c15521d5defe80ffa11f21d029c14c5e73c36
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-23 13:58:33 -0400

    Added urls for expanded test

--- pitch_proto/recommendationTest/urls.py
+++ pitch_proto/recommendationTest/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
 path('test/', views.RecPageOneView , name ='test'),
 path('test2/', views.RecPageTwoView, name = 'test2'),
 path('test3/', views.RecPageThreeView, name = 'test3'),
+path('test4/', views.RecPageFourView , name ='test4'),
+path('test5/', views.RecPageFiveView, name = 'test5'),
+path('test6/', views.RecPageSixView, name = 'test6'),
+path('test7/', views.RecPageSevenView , name ='test7'),
+path('test8/', views.RecPageEightView, name = 'test8'),
+path('test9/', views.RecPageNineView, name = 'test9'),
 path('result/', views.ResultPageView, name = 'result'),
 path('recommendations/', views.RecFinal, name = 'final')
 ]
