5fa38f298f1c2efe1585f0af913a036be1b36bb6
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-27 10:01:31 -0400

    Changed settings page to be more clear

--- pitch_proto/settings/views.py
+++ pitch_proto/settings/views.py
@@ -3,35 +3,61 @@ from .forms import *
 import psycopg2
 import csv
 import pandas as pd
+import recommendationTest.views
+
 
 def settingsPage(request):
 	form = settingForm()
 	return render(request, 'settings.html', {'form' : form})
 
-def settingsUpdate(request):
+def changeUsername(request):
 	user = request.user
 	form = settingForm(request.POST)
 	if form.is_valid():
-		Dark = form.cleaned_data['DarkMode']
-		Explicit = form.cleaned_data['Explicit']
 		username = form.cleaned_data['newUser']
 	conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
 	cursor = conn.cursor()
-	query = 'update accounts_user set dark_mode = ' + str(Dark) + ', explicit = '+ str(Explicit) + ' where id = ' + str(user.id)
-	cursor.execute(query)
 	if username is not '':
 		query = "update accounts_user set username = '" + username + "' where id = " + str(user.id)
 		cursor.execute(query)
+		conn.commit()
+	form = settingForm()
+	return render(request, 'settings.html', {'form' : form})
+
+def toggleDark(request):
+	user = request.user
+	conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+	cursor = conn.cursor()
+	query = 'select dark_mode from accounts_user where id = ' + str(user.id)
+	cursor.execute(query)
+	toggle = cursor.fetchone()[0]
+	if (toggle):
+		query = 'update accounts_user set dark_mode = false where id = ' + str(user.id)
+	else:
+		query = 'update accounts_user set dark_mode = true where id = ' + str(user.id)
+	cursor.execute(query)
 	conn.commit()
 	form = settingForm()
 	return render(request, 'settings.html', {'form' : form})
 
 def settingsReset(request):
-	print('in')
 	form = settingForm()
 	conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
 	cursor = conn.cursor()
-	query = 'update "recommendationTest_preferredmusic" set "danceabilityPreferred" = 0.5175604065506179, "acousticnessPreferred" = 0.5124401243104162, "tempoPreferred" = 116.89153340585992 where id = ' + str(request.user.id)
+	query = 'update "recommendationTest_preferredmusic" set "danceabilityPreferred" = 0.5175604065506179, "acousticnessPreferred" = 0.5124401243104162, "tempoPreferred" = 116.89153340585992, "energyPreferred" = 0.4912925703706923, "instrumentalnessPreferred" = 0.2182890461397754, "livenessPreferred" = 0.22849001657009402, "loudnessPreferred" = -11.807307819952968, "speechinessPreferred" = 0.08092244415462105, "valencePreferred" = 0.49786766966548757 where id = ' + str(request.user.id)
 	cursor.execute(query)
 	conn.commit()
+	id = request.user.id
+	songids = []
+	songids.append(recommendationTest.views.getRecs('danceability', id))
+	songids.append(recommendationTest.views.getRecs('acousticness', id))
+	songids.append(recommendationTest.views.getRecs('tempo', id))
+	songids.append(recommendationTest.views.getRecs('energy', id))
+	songids.append(recommendationTest.views.getRecs('loudness', id))
+	songids.append(recommendationTest.views.getRecs('speechiness', id))
+	songids.append(recommendationTest.views.getRecs('instrumentalness', id))
+	songids.append(recommendationTest.views.getRecs('liveness', id))
+	songids.append(recommendationTest.views.getRecs('valence', id))
+	recommendationTest.views.buildQuickRec(id, songids)
 	return render(request, 'settings.html', {'form' : form})
+
