0c1258d92b17ddc57a4f0edf14c5ff1f6bf8e543
Ryanh99 <71024605+Ryanh99@users.noreply.github.com>
2021-04-29 11:18:27 -0400

    Added popular song feature

--- pitch_proto/pitch_proto/views.py
+++ pitch_proto/pitch_proto/views.py
@@ -3,13 +3,26 @@ import psycopg2
 import pandas as pd
 import recommendationTest.views
 
+def getPopular():
+	conn = psycopg2.connect("host=localhost dbname=pitch_db user=admin password=admin")
+	cursor = conn.cursor()
+	query = 'select track_id from recommender_track where track_popularity > 80 order by random() limit 9'
+	cursor.execute(query)
+	hold = cursor.fetchall()
+	x = 8
+	cleanids = []
+	while(x >= 0):
+		cleanids.append(hold[x][0])
+		x = x - 1
+	return cleanids
 def homepage(request):
 	"""
 	This displays the landing/login page if user is not authenticated. It
 	displays the logged in homepage if they are.
 	"""
+	popids = getPopular()
 	if request.user.is_authenticated:
 		recommendationTest.views.tableEntry(request.user.id)
 		songids = recommendationTest.views.grabQuickRec(request.user.id)
-		return render(request, 'home.html', {'songids' : songids[0][0]})
-	return render(request, 'home.html')
+		return render(request, 'home.html', {'songids' : songids[0][0], 'popids' : popids})
+	return render(request, 'home.html', {'popids' : popids})
